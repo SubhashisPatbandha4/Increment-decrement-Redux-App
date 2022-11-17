a3a75767c876888d69a58579cb87ebf3b2e7fb0d
subhashis patbandha <subhashispatbandha4@gmail.com>
2022-11-17 16:54:35 +0530

    front end design added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+// import "./App.css"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div class=" container-fluid vh-100 bg-warning text-center">
+
+        <h1>Increment/decrement counter</h1>
+        <h4>REDUX-APP</h4>
+        <div className='bg-dark mt-5  text-center space-right2 row m-auto p-5 container-fluid w-50'>
+
+       
+            <button href="#" class=" btn btn-success text-white  "><h1 className='display-1'>+</h1></button>
+            <span name="quantity" class="quantity__input  bg-white " ><h1 className='display-1'>0</h1></span>
+            <button href="#" class="btn btn-danger text-white  " ><h1 className='display-1'>-</h1></button>
+     
+        </div>
+
+      </div>
+    </>
+
+  )
 }
 
-export default App;
+export default App
