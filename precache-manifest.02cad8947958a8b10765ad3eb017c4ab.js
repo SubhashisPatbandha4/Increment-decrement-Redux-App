71f37b696b02b83adbb61f91eff48e23e386a499
subhashis patbandha <subhashispatbandha4@gmail.com>
2022-11-21 23:52:02 +0530

    Updates

---
+++ precache-manifest.02cad8947958a8b10765ad3eb017c4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d768e9475ee7926b23f",
+    "url": "/my-app/static/css/main.0ea52d45.chunk.css"
+  },
+  {
+    "revision": "6d768e9475ee7926b23f",
+    "url": "/my-app/static/js/main.6d768e94.chunk.js"
+  },
+  {
+    "revision": "591bdcbf55c446578560",
+    "url": "/my-app/static/js/1.591bdcbf.chunk.js"
+  },
+  {
+    "revision": "cd13805e97f6eaa1d71c",
+    "url": "/my-app/static/js/2.cd13805e.chunk.js"
+  },
+  {
+    "revision": "f9105491c5c611a266b7",
+    "url": "/my-app/static/js/runtime~main.f9105491.js"
+  },
+  {
+    "revision": "de76f30c6f3cf641992f98fc77d0e1bf",
+    "url": "/my-app/index.html"
+  }
+];
